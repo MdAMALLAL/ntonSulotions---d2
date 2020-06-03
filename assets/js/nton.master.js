91ef1cbbba01a15f67bbfc8c2113d68a86da99e4
Mohammed AMALLAL <amallal.officiel@outlook.com>
2020-06-03 12:37:44 +0100

    d

---
+++ assets/js/nton.master.js
@@ -0,0 +1,53 @@
+
+// $(function () {
+//   flatpickr("#id_signed", {defaultDate: signed,
+//                           enableTime: true,
+//                           allowInput: true,
+//                           dateFormat: "Y-m-d",
+//                         });
+//   });
+// $.ajax({
+//   url: $("#container").attr("data-url"),
+//   dataType: 'json',
+//   success: function (data) {
+//     Highcharts.chart("container", data);
+//   }
+// });
+// $.ajax({
+//   url: $("#container2").attr("data-url"),
+//   dataType: 'json',
+//   success: function (data) {
+//     Highcharts.chart("container2", data);
+//     $(".dateTimeField").val()=
+//   }
+// });
+$(function () {
+  try {
+    flatpickr(".dateTimeField",
+      {defaultDate: $(".dateTimeField").val(),
+      enableTime: true,
+      allowInput: true,
+      time_24hr: true,
+      dateFormat: "Y-m-d H:m",
+    });
+  }
+
+  catch(err) {
+        console.log(err);
+        return;
+      }
+});
+$(function () {
+try {
+  flatpickr(".dateField",
+    {defaultDate: $(".dateField").val(),
+    allowInput: true,
+    dateFormat: "Y-m-d",
+    });
+}
+
+  catch(err) {
+      console.log(err);
+      return;
+}
+});
