6a243b0ade4e1556fda710ff60fbafff94e85d51
Mohammed AMALLAL <amallal.officiel@outlook.com>
2020-06-04 13:10:02 +0100

    ?

--- ntonSulotions/urls.py
+++ ntonSulotions/urls.py
@@ -8,11 +8,11 @@ from . import views
 
 # urlpatterns = [path('?admin/', admin.site.urls), ]
 urlpatterns = i18n_patterns(
-    path('?i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', views.HomePage.as_view(), name="home"),
-    path("?admin/", admin.site.urls),
-    path("?accounts/", include("accounts.urls", namespace="accounts")),
-    path("?accounts/", include("django.contrib.auth.urls")),
-    path("?ticket/", include("solutions.urls", namespace="solutions")),
-    path('?client/', include('clients.urls',namespace='clients'))
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("ticket/", include("solutions.urls", namespace="solutions")),
+    path('client/', include('clients.urls',namespace='clients'))
 )   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
