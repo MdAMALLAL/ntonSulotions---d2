0fb0b8787679a6b7401ce38c68593f52819638f2
Mohammed AMALLAL <amallal.officiel@outlook.com>
2020-06-15 13:28:57 +0100

    fix messages

--- ntonSulotions/settings.py
+++ ntonSulotions/settings.py
@@ -100,24 +100,24 @@ WSGI_APPLICATION = 'ntonSulotions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ntonadvisory$ntonadvisory_db',
-        'USER': 'ntonadvisory',
-        'PASSWORD': 'mYsqL@321qaZ',
-        'HOST': 'ntonadvisory.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ntonadvisory$ntonadvisory_db',
+#         'USER': 'ntonadvisory',
+#         'PASSWORD': 'mYsqL@321qaZ',
+#         'HOST': 'ntonadvisory.mysql.pythonanywhere-services.com',
+#         'OPTIONS': {
+#         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 
 # Password validation
